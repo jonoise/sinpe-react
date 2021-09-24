63894f4fa4ef73b6eae1d65dd1e9a9edf406bf8f
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-24 16:36:51 -0600

    add rollup & babel/react

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
 const distBundle = 'dist/bundle'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.jsx',
   output: [
     {
       file: `${distBundle}.cjs.js`,
@@ -11,5 +15,11 @@ export default {
       file: `${distBundle}.esm.js`,
       format: 'esm'
     }
-  ]
+  ],
+  plugins: [
+    terser(),
+    resolve(),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules' })
+  ],
+  external: ['react']
 }
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const SinpeReact = () => {
+  return <h1>Hello</h1>
+}
