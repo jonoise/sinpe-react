c2949c6599296bd4224c88019aefcaaac3392c7f
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-25 00:21:52 -0600

    add bank numbers

---
+++ src/info/banks.js
@@ -0,0 +1,12 @@
+export const banks = {
+  bn: 2627,
+  bcr: 2276,
+  bac: 70701212,
+  lafise: 9091,
+  davivienda: 70707474,
+  mutual: 70707079,
+  promerica: 62232450,
+  coopealianza: 62229523,
+  ande: 62229524,
+  credecoop: 71984256
+}
