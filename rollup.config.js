25825ed3d0daabb9f1aff21712e6b6d4f5bec1a7
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-26 14:42:36 -0600

    add rollup typescript config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,37 @@
 import babel from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-
+import typescript from '@rollup/plugin-typescript'
 const distBundle = 'dist/bundle'
 
 export default {
-  input: 'src/index.jsx',
+  input: 'src/index.tsx',
   output: [
     {
       file: `${distBundle}.cjs.js`,
       format: 'cjs',
-      exports: 'default'
+      exports: 'default',
+      sourcemap: true
     },
     {
       file: `${distBundle}.esm.js`,
-      format: 'esm'
+      format: 'esm',
+      sourcemap: true
     }
   ],
   plugins: [
     babel({ babelHelpers: 'bundled', exclude: 'node_modules' }),
+    typescript({
+      tsconfig: 'tsconfig.json',
+      sourceMap: true
+    }),
     nodeResolve({ moduleDirectories: ['src'] }),
-    terser()
+    terser({
+      output: { comments: false },
+      compress: {
+        drop_console: true
+      }
+    })
   ],
   external: ['react', 'styled-jsx/style']
 }
