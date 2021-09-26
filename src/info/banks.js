065a7e45b676de0e6ee7f3a7ad0794b89d664571
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-26 13:48:45 -0600

    add banks phone numbers

--- src/info/banks.js
+++ src/info/banks.js
@@ -1,4 +1,9 @@
-export const banks = {
+/*
+Note: Web Archive URL. The real url was removed from Central Bank's SINPE official documentation.
+URL: https://web.archive.org/web/20210824000600/https://www.bccr.fi.cr/sistema-de-pagos/informaci%C3%B3n-general/tarifas-y-comisiones-del-sinpe/comisiones-cobradas-por-las-entidades-financieras/sinpe-m%C3%B3vil
+*/
+
+const banks = {
   bn: 2627,
   bcr: 2276,
   bac: 70701212,
@@ -10,3 +15,5 @@ export const banks = {
   ande: 62229524,
   credecoop: 71984256
 }
+
+export default banks
