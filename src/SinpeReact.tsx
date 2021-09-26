f629dab23273a82d4d6bc3dfd5c16cbc041b4ddd
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-26 14:29:38 -0600

    change extensions to tsx

---
+++ src/SinpeReact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleReact = (props: { className: string }) => {
+  return (
+    <>
+      <button className={props.className}></button>
+    </>
+  )
+}
+
+export default SingleReact
