813372575ad0d8ee6ad97ff6f4798139b45cfe62
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-26 13:49:19 -0600

    add main file

---
+++ src/SinpeReact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SingleReact = (props) => {
+  const { className } = props
+  return (
+    <>
+      <button className={className}></button>
+    </>
+  )
+}
+
+export default SingleReact
