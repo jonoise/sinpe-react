a4d6b42dc2933577da885bb603d4dab561359718
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-26 22:35:25 -0600

    del markup

--- src/SinpeReact.tsx
+++ src/SinpeReact.tsx
@@ -8,16 +8,6 @@ const SinpeReact = (props: SinpeProps) => {
   const { isOpen, onToggle } = UseModal()
   return (
     <>
-      <h1>Info</h1>
-      <ul>
-        <li>customer name: {props.customerOptions.name}</li>
-      </ul>
-      <ul>
-        <li>customer email: {props.customerOptions.email}</li>
-      </ul>
-      <ul>
-        <li>vendor phone: {props.vendorOptions.phone}</li>
-      </ul>
       <button className={btnClass} onClick={onToggle}>
         Pagar con Sinpe
       </button>
