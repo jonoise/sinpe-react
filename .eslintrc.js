88d5156fa87c0d7f28644fe6adb66037ca31fd93
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-03 23:58:16 -0600

    add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "@typescript-eslint/no-implicit-any-catch": ["error", {
+    "allowExplicitAny": true
+  }],
+  "etc/no-implicit-any-catch": ["error", {
+    "allowExplicitAny": true
+  }]
+}
