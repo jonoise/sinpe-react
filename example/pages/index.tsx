db931fe54770a84f9335b77311d6e95cf0f10b57
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-04 00:02:30 -0600

    edit example

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import SinpeReact from 'sinpe-react'
-
 const Home: NextPage = () => {
   return (
       <SinpeReact customer={{email: "email@company.com"}} vendor={{phone:"84019933", callbackUrl:"/whoosh:whoosh"}} />
