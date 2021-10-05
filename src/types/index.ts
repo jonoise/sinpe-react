8596ea6f44a0b89dba1907a5b4a1fb31474ab8e0
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-05 16:16:33 -0600

    edit types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,28 +1,29 @@
-export type VendorOptions = {
-  phone: string
-  callbackUrl: string
-}
-
 export type ConfOptions = {
-  public_key?: string
-  api_key?: string
-}
-
-export type SinpeProps = {
-  btnClass?: string
-  modalClass?: string
-  vendor: VendorOptions
-  conf: ConfOptions
-  order: OrderOptions
-}
-
-export type SinpeModalProps = {
-  modalClass: string
-  onClose: Function
+  public_key: string
+  api_key: string
 }
 
 export type OrderOptions = {
   amount: number
   products?: object[]
   orderId?: string
+  details?: string
+}
+
+export type SinpeProps = {
+  vendorPhoneNumber: string
+  order: OrderOptions
+  conf?: ConfOptions
+  callbackFunction?: Function
+  btnClass?: string
+  modalClass?: string
+}
+
+/////////////////////
+// ↓ MODAL TYPES ↓ //
+/////////////////////
+
+export type SinpeModalProps = {
+  modalClass: string
+  onClose: Function
 }
