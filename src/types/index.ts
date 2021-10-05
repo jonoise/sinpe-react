c03258aacfe24f71f9845fee20b956c504857f5c
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-05 02:54:11 -0600

    add conf & order types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,18 +3,26 @@ export type VendorOptions = {
   callbackUrl: string
 }
 
-export type CustomerOptions = {
-  email: string
+export type ConfOptions = {
+  public_key?: string
+  api_key?: string
 }
 
 export type SinpeProps = {
   btnClass?: string
   modalClass?: string
   vendor: VendorOptions
-  customer: CustomerOptions
+  conf: ConfOptions
+  order: OrderOptions
 }
 
 export type SinpeModalProps = {
   modalClass: string
   onClose: Function
+}
+
+export type OrderOptions = {
+  amount: number
+  products?: object[]
+  orderId?: string
 }
