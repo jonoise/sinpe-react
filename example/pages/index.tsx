96d37084bbe3300746762a5d5227eff44859f538
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-05 23:15:31 -0600

    misc

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,9 +1,26 @@
+import axios from 'axios'
 import type { NextPage } from 'next'
 import SinpeReact from 'sinpe-react'
+
+declare var process : {
+  env: {
+    SINPE_API_KEY: string,
+    SINPE_PUBLIC_KEY: string
+  }
+}
+
 const Home: NextPage = () => {
+  const post_to_my_api = (order:any) => {
+    axios.post("my_api_url")
+  }
+
   return (
-      <SinpeReact customer={{email: "email@company.com"}} vendor={{phone:"84019933", callbackUrl:"/whoosh:whoosh"}} />
+    <SinpeReact 
+    vendorPhoneNumber="84019933" 
+    order={{amount:5000}} 
+    callbackFunction={post_to_my_api} />
   )
+
 }
 
 export default Home
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,18 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import SinpeReact from '../src/index'
+
+const vendorPhoneNumber = '84019933'
 
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-});
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <SinpeReact
+        vendorPhoneNumber={vendorPhoneNumber}
+        order={{ totalAmount: 5500 }}></SinpeReact>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
