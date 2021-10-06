b6b0a139cb8ab3c9bf3f862e21422b31f0d9568c
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-06 00:34:13 -0600

    fixed credentialOptions

--- src/hooks/useCredentialsOptions.ts
+++ src/hooks/useCredentialsOptions.ts
@@ -1,17 +1,18 @@
 import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { CredentialsOptions } from '../types'
+// import { CredentialsOptions } from '../types'
 
 interface ICredentials {
-  public_key: string | null
-  private_key: string | null
+  public_key: string | null | undefined
+  api_key: string | null | undefined
   setCredentials: (credentials: CredentialsOptions | null | undefined) => void
 }
 
 const useCredentialsOptions = create<ICredentials>(
   devtools((set) => ({
     public_key: null,
-    private_key: null,
+    api_key: null,
     setCredentials: (payload) =>
       set((state) => ({
         ...state,
